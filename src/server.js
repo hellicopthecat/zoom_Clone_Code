645820c59b443d3221a8b9a6e5275be2d875550a
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-03-29 18:12:59 +0900

    connect WS & HTTP Server

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ import WebSocket from "ws";
 const app = express();
 const port = 8000;
 
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
@@ -12,11 +15,16 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => {
   res.render("home");
 });
+app.get("/*", (_, res) => {
+  res.redirect("/");
+});
+
+function handleConnect(socket) {
+  console.log(socket);
+}
+
+wss.on("connection", handleConnect);
 
 const handleListen = () =>
   console.log(`✅ Listening on http://localhost:${port} ✅`);
-
-const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
-
 server.listen(port, handleListen);
