5ab7acb767f811bb095f550bdfc9e5713c5efad7
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-03-29 17:48:11 +0900

    Setting Websocket server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 const port = 8000;
@@ -7,11 +9,14 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.render("home");
 });
 
 const handleListen = () =>
   console.log(`✅ Listening on http://localhost:${port} ✅`);
 
-app.listen(port, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+server.listen(port, handleListen);
