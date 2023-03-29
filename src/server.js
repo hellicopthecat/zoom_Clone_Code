c7aa7327a9dcb4527bfed1aba6b9c40d87ddb8c9
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-03-29 17:02:37 +0900

    Frontend SetUp

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import express from "express";
 
 const app = express();
+const port = 8000;
 
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 
-console.log("hello");
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const handleListen = () =>
+  console.log(`✅ Listening on http://localhost:${port} ✅`);
+
+app.listen(port, handleListen);
