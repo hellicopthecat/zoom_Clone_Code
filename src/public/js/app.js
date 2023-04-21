e570f1fc882341c827866d2aaedd11731112ee82
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-04-21 16:20:37 +0900

    connect & disconnect message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,18 +8,32 @@ message.hidden = true;
 
 let roomNmae;
 
+function addMessage(msg) {
+  const ul = message.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = msg;
+  ul.appendChild(li);
+}
+
 function showRoom() {
   room.hidden = true;
   message.hidden = false;
   const h3 = message.querySelector("h3");
   h3.innerText = `Room Name is ${roomNmae}`;
 }
-//[1] emit으로 백엔드와 연결 하고 인풋의 값과 실행함수를 보내준다
+//[1-1] emit으로 백엔드와 연결 하고 인풋의 값과 실행함수를 보내준다
 function handleSubmit(event) {
   event.preventDefault();
   socket.emit("room_create", roomInput.value, showRoom);
   roomNmae = roomInput.value;
   roomInput.value = "";
 }
-
 room.addEventListener("submit", handleSubmit);
+
+//[1-5] [1-4]구현 addMessage func 참조
+socket.on("welcome", () => {
+  addMessage("SOMEONE JOINED");
+});
+socket.on("bye", () => {
+  addMessage("SOMEONE LEFT");
+});
--- src/server.js
+++ src/server.js
@@ -30,11 +30,20 @@ wss.on("connection", (socket) => {
   socket.onAny((event) => {
     console.log(`socket event : ${event}`);
   });
+
   // 방만들기 소켓을 프론트와 연동한다. 인자로는 방제목과 실행함수(꼭 아니여도 됨)를 프론트에서 받을 수 있게 한다.[1]
   socket.on("room_create", (roomname, done) => {
     //[1-2] 프론트의 받은 값으로 방에 들어가게 한다.
     //[1-3] 방이 생성되면 각 방마다 ID가 자동생성된다 (console.log(socket.id))
     socket.join(roomname);
     done();
+    //[1-4] 방에 들어온 사람들에게 웰컴메세지를 보이기
+    socket.to(roomname).emit("welcome");
+  });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => {
+      socket.to(room).emit("bye");
+    });
   });
 });
